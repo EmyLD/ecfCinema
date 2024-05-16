2c4314a4d96fc96f877eadeb1b71b8ca3ff33474
Benway Teh <teh.benway@gmail.com>
2024-05-16 10:52:45 +0200

    ajout de addOne dans RoleDao

--- model/repository/RoleDao.php
+++ model/repository/RoleDao.php
@@ -7,7 +7,7 @@ require_once './model/entity/Actor.php';
 class RoleDao
 {
 
-
+    //Récupère tous les rôles
     public static function getAll()
     {
         $query = BDD->prepare('SELECT * FROM role');
@@ -19,7 +19,7 @@ class RoleDao
         return $roles;
     }
 
-     //Récupérer plus d'info sur 1 acteur
+     //Récupére 1 rôle
      public static function getOne(int $id): Role
      {
          $query = BDD->prepare('SELECT * FROM role WHERE id = :id_role');
@@ -28,20 +28,19 @@ class RoleDao
          return new Role($data['id'], $data['fk_movie'], $data['fk_actor'], $data['character']);
      }
 
+     //Récupère le/les rôles d'un film
      public static function getRoleMovie(int $id, $fk_movie): Role
      {
-         $query = BDD->prepare('SELECT * FROM role INNER JOIN movie ON role.fk_movie = movie.id WHERE id = :id_role' );
+         $query = BDD->prepare('SELECT * FROM role INNER JOIN movie ON role.fk_movie = movie.id WHERE role.id = :id_role AND movie:id = :fk_movie');
          $query->execute(array(':id_role' => $id, ':fk_movie' => $fk_movie));
          $data = $query->fetch();
          return new Role($data['id'], $data['fk_movie'], $data['fk_actor'], $data['character']);
      }
 
-
-     
-
-    // public static function addOne($data): bool
-    // {
-       
-    // }
-
+     //Ajoute un rôle dans la BDD
+    public static function addOne(int $fk_movie, int $fk_actor, string $character)
+    {
+       $query = BDD->prepare('INSERT INTO role (fk_movie, fk__actor, character) VALUES (:fk_movie, :fk_actor, :character)');
+       $query->execute(array(':fk_movie' => $fk_movie, ':fk_actor' => $fk_actor, ':character' => $character));
+    }
 }
