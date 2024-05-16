016bc2a71f756383aaf3d4a4fa6e271500d7e81b
Dradoke <nathan.gaudouin76190@gmail.com>
2024-05-16 19:27:55 +0200

    accueil presque fini, manque hauteurs abolute image et ajout header

--- controller/home.php
+++ controller/home.php
@@ -3,7 +3,6 @@ require_once './model/repository/connexion.php';
 require_once './model/repository/MovieDao.php';
 require_once './model/repository/ActorDao.php';
 require_once './model/repository/RoleDao.php';
-
 require_once './model/entity/Movie.php';
 require_once './model/entity/Actor.php';
 require_once './model/entity/Role.php';
