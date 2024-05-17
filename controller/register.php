c9a59750e4268dba6b289e4f31ae2ec4041b2588
Emy <145982137+EmyLD@users.noreply.github.com>
2024-05-17 15:29:01 +0200

    register ok

--- controller/register.php
+++ controller/register.php
@@ -7,19 +7,28 @@ use Model\repository\UserDAO;
 echo $twig->render('register.html.twig');
 
   
-
 if(isset($_POST['email']) && isset($_POST['password']) && isset($_POST['username']) ){
+    $patternUsername =  '/^[a-zA-Z]{3,20}$/';
+    $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+    $username = preg_match($patternUsername, $_POST['username']);   
     try {
-        $hash = hash('sha256',$_POST['password']);
-        $isAdded = UserDao::addOne($_POST['username'], $_POST['email'], $hash);
-        
-        if ($isAdded) {
-            $_SESSION['username'] = $username;
-            header('Location: /ecfCinema/home');
-            exit();
+        if($email && $username === 1) {
+            $hash = hash('sha256',$_POST['password']);
+            $cleanUsername = htmlspecialchars($_POST['username']);
+            $capUsername= ucfirst($cleanUsername);
+            $isAdded = UserDao::addOne($capUsername, $_POST['email'], $hash);
+            
+            if ($isAdded) {
+                $_SESSION['username'] = $capUsername;
+                header('Location: /ecfCinema/login');
+                exit();
+            } else {
+                echo $twig->render('register.html.twig', ['error' => $error]);
+            } 
         } else {
-            echo $twig->render('register.html.twig', ['error' => $error]);
-        } 
+            null;
+        }
+        
     } catch (Exception $e) {
         die ('Error : ' . $e->getMessage());
     }
