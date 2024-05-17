7bcb44767a23043134c3a08a00cd017f43c5ca19
Emy <145982137+EmyLD@users.noreply.github.com>
2024-05-17 12:03:30 +0200

    header getting data from session if it's set beforehand, else returning a msg

--- controller/header.php
+++ controller/header.php
@@ -1,3 +1,10 @@
 <?php
 
-echo $twig->render('header.html.twig');
+
+$session = null;
+
+if(isset($_SESSION['username'])){
+    $session = $_SESSION['username'];
+}
+
+echo $twig->render('header.html.twig', ['session' => $session]);
